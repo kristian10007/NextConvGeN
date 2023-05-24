18c5db61a139ee2f22a267415b76ca49803ed396
Kristian <kristian.schultz@uni-rostock.de>
2023-05-24 12:24:47 +0200

    Fixed library problems.

--- docker/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /benchmark/data
-python3 run_all_exercises.py
--- library/generators/__init__.py
+++ library/generators/__init__.py
@@ -1,3 +1,5 @@
 from library.generators.SimpleGan import SimpleGan
-from library.generators.ConvGeN import ConvGeN
 from library.generators.Repeater import Repeater
+from library.generators.ConvGeN import ConvGeN
+from library.generators.NextConvGeN import NextConvGeN
+from library.generators.XConvGeN import XConvGeN
